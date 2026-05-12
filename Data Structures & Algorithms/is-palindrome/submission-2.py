da6321ec49707028abdc522f263ac314efc9ca9a
Ibrahim Ahmed <ibrahimahmed443@gmail.com>
2026-05-13 00:35:13 +0500

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ''
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        
+        return newStr == "".join(reversed(newStr))
+    
