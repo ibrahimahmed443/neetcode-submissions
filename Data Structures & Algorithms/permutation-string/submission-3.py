be88707a4bf60b71075061b197c5332a2a71b2f1
Ibrahim Ahmed <ibrahimahmed443@gmail.com>
2026-05-21 20:35:53 +0500

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,33 @@
+from itertools import permutations
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        countS1 = self.findCount(s1)
+
+        l = 0
+        counts = {}
+        for r in range(len(s1) - 1, len(s2)):
+            seq = s2[l:r+1]
+
+            if l == 0:
+                countSeq = self.findCount(seq)
+            else:
+                countSeq[s2[r]] = 1 + countSeq.get(s2[r], 0)
+                if countSeq[s2[l-1]] > 1:
+                    countSeq[s2[l-1]] -= 1
+                else:
+                    countSeq.pop(s2[l-1])
+
+            if countSeq == countS1:
+                return True
+            
+            l += 1
+
+        return False
+
+    def findCount(self, s):
+        count = {}
+        for c in s:
+            count[c] = 1 + count.get(c, 0)
+
+        return count
