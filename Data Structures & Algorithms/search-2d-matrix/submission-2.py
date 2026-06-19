8846a34eac21c08cb4e7ffe71a745909a94a16f3
Ibrahim Ahmed <ibrahimahmed443@gmail.com>
2026-06-19 21:40:46 +0500

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        start = 0
+        end = m * n - 1
+
+        while start <= end:
+            mid = start + ((end - start) // 2)
+            num = self.getNumberAtIndex(matrix, mid, n)
+            if num == target:
+                return True
+            
+            if num < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        
+        return False
+
+    def getNumberAtIndex(self, matrix, index, n):
+        row, col = index // n, index % n
+        return matrix[row][col]
