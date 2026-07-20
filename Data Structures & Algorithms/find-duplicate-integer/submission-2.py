ec2984bb4a0ee0e7577f00920ff64d3807d4933f
Ibrahim Ahmed <ibrahimahmed443@gmail.com>
2026-07-20 20:01:32 +0500

    Remove: find-duplicate-integer - submission-2

--- Data Structures & Algorithms/find-duplicate-integer/submission-2.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        s = set()
-        for num in nums:
-            if num in s:
-                return num
-            s.add(num)
-
